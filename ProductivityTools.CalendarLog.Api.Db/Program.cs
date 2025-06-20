0890069d341e75bd9678dea326c5fa9a10d00ecb
Pawel Wujczyk <pwujczyk@gmail.com>
2025-06-20 22:35:33 +0200

    cursor generated dbup project

--- ProductivityTools.CalendarLog.Api.Db/Program.cs
+++ ProductivityTools.CalendarLog.Api.Db/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using DbUp;
+using ProductivityTools.MasterConfiguration;
+
+IConfigurationRoot configuration =
+                new ConfigurationBuilder()
+                .AddMasterConfiguration(configName: "ProductivityTools.CalendarLog.Api.json", force: true)
+                .Build();
+
+var masterConnectionString = configuration["ConnectionString"];
+var connectionString = args.FirstOrDefault() ?? masterConnectionString;
+
+EnsureDatabase.For.SqlDatabase(connectionString);
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
